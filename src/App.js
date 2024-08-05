dbfd808a3fbcdfac86643ef19b2c19ccaec9dd63
barucgs <barucgs@gmail.com>
2024-08-05 10:44:50 -0600

    FIX app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import ForgotPassword from './components/Auth/ForgotPassword';
+import Dashboard from './components/Dashboard';
+import Invitations from './components/Invitations';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/invitations" element={
+        <PrivateRoute>
+          <Invitations />
+        </PrivateRoute>
+      } />
+      <Route path="/" element={<Login />} />
+    </Routes>
   );
 }
 
 export default App;
+
